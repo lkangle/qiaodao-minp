9806099102e27fc8528b54dde45f0dbaa493e3a1
likangle <likangle@growingio.com>
2021-04-23 22:49:26 +0800

    feat: 初始化了两个页面

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,11 +1,27 @@
 export default {
   pages: [
-    'pages/index/index'
+    'pages/index/index',
+    'pages/user/index'
   ],
   window: {
     backgroundTextStyle: 'light',
     navigationBarBackgroundColor: '#fff',
     navigationBarTitleText: 'WeChat',
     navigationBarTextStyle: 'black'
+  },
+  tabBar: {
+    backgroundColor: '#fff',
+    color: '#000',
+    selectedColor: '#206d5e',
+    list: [
+      {
+        text: '首页',
+        pagePath: 'pages/index/index'
+      },
+      {
+        text: '我的',
+        pagePath: 'pages/user/index'
+      }
+    ]
   }
 }
---
+++ src/pages/user/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from '@tarojs/components'
+
+const Index = () => {
+  return (
+    <View>
+      <Text>用户中心页面</Text>
+    </View>
+  );
+};
+
+export default Index;
